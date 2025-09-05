c5266045b5454dfa3107e02c31d36d00038c6dfe
rdheerajp <44521383+rdheerajp@users.noreply.github.com>
2025-09-05 18:17:41 +0000

    Deploying to gh-pages from @ rdheerajp/rdheerajp.github.io@2f39440086e6fc791af003c2756657da1052fce0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -69,51 +69,11 @@ ninja.data = [{
           section: "News",},{id: "news-it-took-me-an-year-to-get-this-rolling-going-public-today",
           title: 'It took me an year to get this rolling. Going public today.',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-cfd-solver",
+          title: 'cfd solver',
+          description: "simulating fluids with tensor networks",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/tn_cfd/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
